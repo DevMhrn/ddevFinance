ec80785c38e80936ccb9dfaa81208591d67a0cde
DevMhrn <debashismaharana7854@gmail.com>
2024-12-28 19:04:11 +0530

    Intialized the project
    Signed-off-by: DevMhrn <debashismaharana7854@gmail.com>

---
+++ server/config/dbConfig.js
@@ -0,0 +1,11 @@
+import pg from 'pg';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+const { Pool } = pg;
+
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+    
---
+++ server/index.js
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import express from 'express';
+import { configDotenv } from 'dotenv';
+configDotenv();
+import Routes from './routes/index.js';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors('*')); // Enable All CORS Requests from Anywhere
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/v1', Routes);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
+    console.log(`Example app listening on port ${PORT}`);
+});
